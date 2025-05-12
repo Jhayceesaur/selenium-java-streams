f40d6e71301ec282eaf552cbc23eb62aada68d3f
John Carlo B. Alarva <jc.alarva1994@gmail.com>
2025-05-12 13:07:47 +0800

    Add files via upload

---
+++ CustomSeleniumStreamsMapper.java
@@ -0,0 +1,34 @@
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class CustomSeleniumStreamsMapper 
+{
+	public static void main(String[] args) 
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/offers");
+		
+		driver.findElement(By.xpath("//span[normalize-space()='Veg/fruit name']")).click(); //Clicking the web element to sort the values within the table
+		List <WebElement> itemList = driver.findElements(By.xpath("//tr/td[1]")); //Locate values within the table
+		List <String> origItemList = itemList.stream().map(s->s.getText()).collect(Collectors.toList()); //Store located values within the table thru streams and storing it to a new list (origItemList)
+		List <String> sortedItemList = origItemList.stream().sorted().collect(Collectors.toList()); //New list stored will be sorted and storing it again to a new list (sortedItemList)
+		Assert.assertTrue(origItemList.equals(sortedItemList)); //Comparing lists if values match (origItemList and sortedItemList)
+		
+		//Implement custom method
+		List <String> price = itemList.stream().filter(s->s.getText().contains("Beans")).map(s->getItePrice(s)).collect(Collectors.toList());
+		price.forEach(a->System.out.println(a));
+	}
+
+	private static String getItePrice(WebElement s) 
+	{
+		String priceValue = s.findElement(By.xpath("following-sibling::td[1]")).getText(); //Get values of the sibling element (price)
+		return priceValue; 
+	}
+}
